de7889c263da966d5aeb7de6c8844503bf624182
Steve Aldworth <s.aldworth@tvdsb.ca>
2022-05-12 14:39:57 -0400

    Finished Q4

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace Advanced_Looping_Solutions
     {
         static void Main(string[] args)
         {
-            LoopQ2();
+            LoopQ4();
             Console.ReadLine();
         }
 
@@ -18,23 +18,42 @@ namespace Advanced_Looping_Solutions
         public static void LoopQ1()
         {
             for (int i = 0; i < 10; i++)
-                Console.Write("*");
+                Console.Write("* ");
         }
 
         public static void LoopQ2()
         {
             for (int i = 0; i < 10; i++)
-                Console.Write("*");
+                Console.Write("* ");
             Console.WriteLine();
 
             for (int i = 0; i < 5; i++)
-                Console.Write("*");
+                Console.Write("* ");
             Console.WriteLine();
 
             for (int i = 0; i < 20; i++)
-                Console.Write("*");
+                Console.Write("* ");
             Console.WriteLine();
+        }
 
+        public static void LoopQ3()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                    Console.Write("* ");
+                Console.WriteLine("");
+            }
+        }
+
+        public static void LoopQ4()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                    Console.Write("* ");
+                Console.WriteLine("");
+            }
         }
     }
 }
