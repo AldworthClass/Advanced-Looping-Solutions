4db304cdb4ef81663880241b8db315156b423d19
Steve Aldworth <s.aldworth@tvdsb.ca>
2022-05-12 14:34:32 -0400

    First 2 problems implemented

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Looping_Solutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoopQ2();
+            Console.ReadLine();
+        }
+
+        // Question 1
+        public static void LoopQ1()
+        {
+            for (int i = 0; i < 10; i++)
+                Console.Write("*");
+        }
+
+        public static void LoopQ2()
+        {
+            for (int i = 0; i < 10; i++)
+                Console.Write("*");
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+                Console.Write("*");
+            Console.WriteLine();
+
+            for (int i = 0; i < 20; i++)
+                Console.Write("*");
+            Console.WriteLine();
+
+        }
+    }
+}
